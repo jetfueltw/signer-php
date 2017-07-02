323be324d63e577f9e7e49743005f955723af56e
Cooper <jungle1900@gmail.com>
2017-07-02 17:31:43 +0800

    define interface

--- src/Signer.php
+++ src/Signer.php
@@ -5,25 +5,7 @@ namespace Jetfuel\Signer;
 class Signer
 {
     /**
-     * Generate HMAC-SHA256 signature only include timestamp.
-     * This signature CANNOT make sure your data is correct.
-     * https://en.wikipedia.org/wiki/Man-in-the-middle_attack
-     *
-     * @param string $appId
-     * @param string $appSecret
-     * @param int $timestamp
-     * @return string
-     */
-    public static function signExceptContent($appId, $appSecret, $timestamp)
-    {
-        $baseString = $appId.'&'.$timestamp;
-
-        return self::hmacHash($baseString, $appSecret);
-    }
-
-    /**
-     * Generate HMAC-SHA256 signature.
-     * Content-Type: application/x-www-form-urlencoded
+     * Generate signature.
      *
      * @param string $appId
      * @param string $appSecret
@@ -31,59 +13,60 @@ class Signer
      * @param string $method
      * @param string $baseUrl
      * @param array $parameters
+     * @param mixed $content
      * @return string
      */
-    public static function signFormContent($appId, $appSecret, $timestamp, $method, $baseUrl, $parameters = [])
+    public static function sign($appId, $appSecret, $timestamp, $method, $baseUrl, $parameters = [], $content = '')
     {
         $method = strtoupper($method);
-        $baseUrl = self::urlEncode(rtrim(strtok($baseUrl, '?'), '/'));
+        $baseUrl = self::urlEncode(self::removeQueryString($baseUrl));
         $parameterString = self::buildParameterString($parameters);
+        $content = self::removeLineBreak($content);
 
-        $baseString = $appId.'&'.$timestamp.'&'.$method.'&'.$baseUrl.'&'.$parameterString;
-
-        return self::hmacHash($baseString, $appSecret);
-    }
-
-    /**
-     * @param string $appId
-     * @param string $appSecret
-     * @param int $timestamp
-     * @param string $method
-     * @param string $baseUrl
-     * @param array $querys
-     * @param string $body
-     * @return string
-     */
-    public static function signJsonContent($appId, $appSecret, $timestamp, $method, $baseUrl, $querys = [], $body = '')
-    {
-        $method = strtoupper($method);
-        $baseUrl = self::urlEncode(rtrim(strtok($baseUrl, '?'), '/'));
-        $queryString = self::buildParameterString($querys);
-        $body = preg_replace('/\s+/', '', $body);
-
-        $baseString = $appId.'&'.$timestamp.'&'.$method.'&'.$baseUrl.'&'.$queryString.'&'.$body;
-
-        var_dump($baseString);
+        $baseString = $appId.'&'.$timestamp.'&'.$method.'&'.$baseUrl.'&'.$parameterString.'&'.$content;
 
         return self::hmacHash($baseString, $appSecret);
     }
 
     /**
-     * Compare the two signatures is match.
+     * Compare signatures is match, with timing attack safe string comparison
      *
      * @param string $signature
-     * @param string $id
-     * @param string $secret
+     * @param string $appId
+     * @param string $appSecret
      * @param int $timestamp
+     * @param string $method
      * @param string $baseUrl
      * @param array $parameters
+     * @param mixed $content
      * @return bool
      */
-    // public function compare($signature, $id, $secret, $timestamp, $baseUrl, $parameters = [])
-    // {
-    //     // return hash_equals(base64_encode(hash_hmac('sha256', $body, $channelSecret, true)), $signature);
-    //     // return $this->sign($id, $secret, $timestamp, $baseUrl, $parameters) === $signature;
-    // }
+    public function compare($signature, $appId, $appSecret, $timestamp, $method, $baseUrl, $parameters = [], $content = '')
+    {
+        return hash_equals(self::sign($appId, $appSecret, $timestamp, $method, $baseUrl, $parameters, $content), $signature);
+    }
+
+    /**
+     * Remove line breaks and whitespace.
+     *
+     * @param $content
+     * @return mixed
+     */
+    private static function removeLineBreak($content)
+    {
+        return preg_replace('/\s+/', '', $content);
+    }
+
+    /**
+     * Remove query string and slash end.
+     *
+     * @param string $url
+     * @return string
+     */
+    private static function removeQueryString($url)
+    {
+        return rtrim(strtok($url, '?'), '/');
+    }
 
     /**
      * URL encode according to RFC 3986 specifications.
